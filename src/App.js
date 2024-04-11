d84f554c7e059fc103f60a86ee360aab8c1f89b4
paraggaikwad18 <paraggaikwad0007@gmail.com>
2024-04-11 11:32:29 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+
+import { useState } from 'react';
+import './App.css';
+// import About from './component/About';
+import Navbar from './component/Navbar';
+import TextForm from './component/TextForm';
+import Alert from './component/Alert';
+
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route} from "react-router-dom";
+
+function App() {
+  //to set the mode
+  const [mode,setMode] = useState('light')
+  
+  //to set the alert
+  const [alert,setAlert] = useState(null)
+  
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  }
+  const toggleMode = () => {
+    if(mode==='light'){
+      setMode('dark')
+      document.body.style.backgroundColor='#042743'
+      showAlert("Dark mode Enabled","success")
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor='white'
+      showAlert("Light mode Enabled","success")
+    }
+  }
+  return (
+  <>
+  {/* <Router> */}
+    {/* <Navbar title= "textUtils" about = "About TextUtils" /> */}
+    <Navbar title= "textUtils" mode = {mode} toggleMode={toggleMode}/>
+    {<Alert alert={alert} />}
+    <div className="container my-3">
+    {/* <Switch> */}
+          {/* <Route path="/about">
+            <About/>
+          </Route>
+          <Route path="/"> */}
+            <TextForm showAlert= {showAlert} heading = "Enter Amazing Text to Analyze" mode = {mode}/>
+          {/* </Route>
+        </Switch>    */}
+    </div>
+  {/* </Router> */}
+  </>
+  );
+}
+
+export default App;
---
+++ src/component/TextForm.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+
+export default function TextForm(props) {
+    const handleUpCase = ()=>{
+        console.log("Clicked on Uppcase");
+        let UpperCase = text.toUpperCase();
+        setText(UpperCase);
+        props.showAlert("Converted to Upper Case!","success")
+    }
+
+    const handleLoCase = ()=>{
+        console.log("Clicked on Uppcase");
+        let Lowercase = text.toLowerCase();
+        setText(Lowercase);
+        props.showAlert("Converted to Lower Case!","success")
+    }
+
+    const handleSeCase = ()=>{
+        let SentenceCase = text.toLowerCase().charAt(0).toUpperCase() + text.slice(1);
+        setText(SentenceCase);
+        props.showAlert("Converted to Sentence Case!","success")
+    }
+
+    const handleClear = ()=>{
+        let clear = " ";
+        setText(clear);
+        props.showAlert("Text cleared!","success")
+    }
+
+    const handleOnchange = (event)=>{
+        console.log("On change");
+        setText(event.target.value);
+    }
+
+    const [text,setText] = useState("Enter your Text");
+  return (
+    <>
+    <div className='container' style={{color: props.mode==='dark'?'white':'#042743'}}>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+        <textarea className="form-control" value = {text} onChange={handleOnchange} style={{backgroundColor: props.mode==='light'?'white':'#042743', color:props.mode==='dark'?'white':'#042743' }} id="myBox" rows="8"></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpCase} >Convert to Upper case</button>
+        <button className="btn btn-primary mx-2" onClick={handleLoCase} >Convert to Lower case</button>
+        <button className="btn btn-primary mx-2" onClick={handleSeCase} >Convert to Sentence case</button>
+        <button className="btn btn-primary mx-2" onClick={handleClear} >Clear</button>
+    </div>
+    <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
+        <h2>Summary</h2>
+        <p>{text.split(" ").length} Words {text.length} Characters</p>
+        <p>{0.008*text.split(" ").length} Minutes to read the content</p>
+        <h2>Preview</h2>
+        <p>{text.length>0?text:"Enter something to Preview"}</p>
+    </div>
+    </>
+  )
+}
